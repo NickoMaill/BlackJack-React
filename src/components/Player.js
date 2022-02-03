a65dd6f98741791e26c87e9f1332542e9894fdc9
jlmoer <jlmoerdijk@gmail.com>
2022-02-03 14:36:26 +0100

    Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,9 +3,11 @@ import React from 'react';
 class Player extends React.Component {
 
     render() {
-        <div>
-            <p>Player</p>
-        </div>
+        return (
+            <div>
+                <p>Player</p>
+            </div>
+        )
     }
 }
 
