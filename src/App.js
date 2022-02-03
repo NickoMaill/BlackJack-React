6bd130cbddbb345dd2ec88d0e936551bf585d872
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-03 12:06:11 +0100

    TEST merge

--- src/App.js
+++ src/App.js
@@ -9,11 +9,8 @@ class App extends React.Component {
 
   render() {
     <p> COMPONENT APP</p>;
-<<<<<<< HEAD
-    <p> COMPONENT APP2 bonjour nous travaillons salement</p>;
-=======
     <p> COMPONENT APP2 bonjour nous travaillons bien</p>;
->>>>>>> 400283612dbb74c8799feecfed56cb3fbe82e7bf
+    <p>LOl</p>;
   }
 }
 
