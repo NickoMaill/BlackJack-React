9fc5efaa8f8e2f2f043f99ec2fd1e0899c82e202
Djibril Samassa <djibrilsamassa.pro@gmail.com>
2022-02-03 14:26:48 +0100

    Score commit

---
+++ src/components/Score.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Score extends React.Component {
+
+    render(){
+        return(
+
+
+            <p>Score</p>
+
+
+        )
+    };
+}
+
+export default Score;
