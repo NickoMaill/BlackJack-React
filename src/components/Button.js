62b1f1d0ed8c1da8d90036c09719ed7670fa9b36
Nicko <nicomaillols@gmail.com>
2022-02-07 10:01:44 +0100

    props button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 class Button extends React.Component {
+    
 
     render() {
 
         return(
 
-            <p>Button</p>
+            <div>
+
+                <button onClick={this.props.onClick} type={this.props.type} className={this.props.class}>{this.props.children} 'test'</button>
+
+            </div>
 
         )
 
     }
-    
+
 }
 
 export default Button;
